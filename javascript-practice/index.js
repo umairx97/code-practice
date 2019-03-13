75f8384bfeccddce5e1b6fd6efb4d15675c8531f
umairx97 <umair.bajwa97@hotmail.com>
2019-03-14 04:05:27 +0500

    Toggle Todo Functionality

--- javascript-practice/index.js
+++ javascript-practice/index.js
@@ -176,6 +176,10 @@ function addTodoToDom(todo) {
   const node = document.createElement("li");
   const text = document.createTextNode(todo.name);
   node.appendChild(text);
+  node.style.textDecoration = todo.complete ? "line-through" : "none";
+  node.addEventListener("click", () => {
+    store.dispatch(toggleTodoAction(todo.id));
+  });
 
   document.getElementById("todos").appendChild(node);
 }
