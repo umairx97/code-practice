86c6efb3ec3357129ee46a258651d779bc523da5
umairx97 <umair.bajwa@hotmail.com>
2019-03-19 01:40:10 +0500

    18-march

---
+++ javascript-practice/eighteen-march.js
@@ -0,0 +1,61 @@
+// const getName = name => {
+//   console.log(name);
+// };
+
+// const getAge = age => {
+//   console.log(age);
+// };
+
+// const getProfession = profession => {
+//   console.log(profession);
+// };
+
+// const getStudy = study => {
+//   console.log(study);
+// };
+
+// const users = {
+//   name: "umiar",
+//   profession: "developer",
+//   age: 21
+// };
+
+// const { profession } = users;
+
+// console.log(profession);
+
+// ES5
+// function myFunc(name) {
+//   console.log(name);
+// }
+
+// myFunc("umair");
+
+// ES6
+// const getName = name => {
+//   console.log(name);
+// };
+
+// const getName = (name, age) => console.log(name, age)
+
+// getName('ashir', 21)
+
+// const a = 10;
+// const b = 10;
+
+// iserror = (a, b) => {
+//   return a === b ? true : false;
+// };
+
+
+
+// if (a === b) {
+//   console.log("Condition works");
+// } else {
+//   console.log("Condition does not work");
+// }
+
+
+// const c = a==b ? 'good work' : 'Not good work';
+
+// console.log(c)
