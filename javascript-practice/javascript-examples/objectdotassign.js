4d0ccff995e928380e1d3b8b9f0a4a10b6ee7e3f
umairx97 <umair.bajwa@hotmail.com>
2019-03-29 15:55:33 +0500

    Javascript examples

---
+++ javascript-practice/javascript-examples/objectdotassign.js
@@ -0,0 +1,37 @@
+// Run one part at a time
+// const obj1 = {
+//   name: "Umair",
+//   profession: "Developer",
+//   age: 21
+// };
+
+// const obj2 = {
+//   occupation: 'react-developer'
+// };
+
+// const abc = Object.assign({}, obj1,obj2);
+
+// console.log(abc);
+
+// ************************************************
+
+// const obj1 = {
+//   name: "umair",
+//   profession: "developer",
+//   age: 21
+// };
+
+// const obj2 = {
+//   hobbies: ["Coding", "UI Design", "UX Design"]
+// };
+
+// const obj3 = {
+//   interests: ["Gaming", "Building Apps"]
+// };
+
+// Object.assign(obj1, obj2, obj3);
+
+// console.log(obj1.interests);
+// console.log(obj1["hobbies"]);
+
+// *************************************************
