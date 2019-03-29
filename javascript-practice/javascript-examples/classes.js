8d9b036b0ff0b228337dd47860a2276f16ed631b
umairx97 <umair.bajwa@hotmail.com>
2019-03-29 18:01:32 +0500

    Added Classes example

---
+++ javascript-practice/javascript-examples/classes.js
@@ -0,0 +1,16 @@
+class Message {
+  static flag = "Pakistan";
+
+  constructor(name1, name2) {
+    this.name1 = name1;
+    this.name2 = name2;
+  }
+
+  printMsg = () => {
+    console.log(`Welcome ${this.name1} ${this.name2} to ${Message.flag}`);
+  };
+}
+
+const Umair = new Message("Umair", "Ahmed");
+
+Umair.printMsg();
