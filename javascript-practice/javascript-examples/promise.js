f693584437e66ce049006fe1a71f9caa3b07f4da
umairx97 <umair.bajwa@hotmail.com>
2019-04-02 00:17:24 +0500

    Added Test

--- javascript-practice/javascript-examples/promise.js
+++ javascript-practice/javascript-examples/promise.js
@@ -1,14 +1,14 @@
-const anyFunction = () => {
-  return Math.floor(Math.random() * 10) % 2 === 0 ? true : false;
-};
+// const anyFunction = () => {
+//   return Math.floor(Math.random() * 10) % 2 === 0 ? true : false;
+// };
 
-const resolveFunction = () => console.log("success");
-const rejectFunction = () => console.log("failure");
+// const resolveFunction = () => console.log("success");
+// const rejectFunction = () => console.log("failure");
 
-// Promise will output failure if no time is given to calculations
-const result = new Promise((res, rej) => {
-  setTimeout(() => (anyFunction() ? res() : rej()), 1000);
-});
+// // Promise will output failure if no time is given to calculations
+// const result = new Promise((res, rej) => {
+//   setTimeout(() => (anyFunction() ? res() : rej()), 1000);
+// });
 
-result.then(resolveFunction).catch(rejectFunction);
-console.log("Hello world");
+// result.then(resolveFunction).catch(rejectFunction);
+// console.log("Hello world");
