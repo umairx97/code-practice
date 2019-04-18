e7e7af7f3382f410b4d2e2ee573d34273be8180c
umairx97 <umair.bajwa@hotmail.com>
2019-04-19 02:12:40 +0500

    Added Tests and chalk

--- javascript-practice/simple-js-blockchain/main.js
+++ javascript-practice/simple-js-blockchain/main.js
@@ -1,4 +1,5 @@
 const SHA256 = require("crypto-js/sha256");
+const chalk = require("chalk");
 
 class Block {
   constructor(index, timestamp, data, previousHash = "") {
@@ -37,10 +38,35 @@ class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+
+  isChainValid() {
+    for (var i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
 
 let JScoin = new Blockchain();
 
-JScoin.addBlock(new Block(1, "19/04/2019", { name: "Umair", amount: 100 }));
+JScoin.addBlock(new Block(1, "17/04/2019", { name: "Umair", amount: 100 }));
 JScoin.addBlock(new Block(1, "19/04/2019", { name: "Asad", amount: 100 }));
+
+
+// TESTS for validating chain
+// JScoin.chain[1].data = { amount: 150 };
+// JScoin.chain[1].hash = JScoin.chain[1].calculateHash();
+
+console.log("Is Blockchain Valid? " + chalk.green(JScoin.isChainValid()));
+
 console.log(JSON.stringify(JScoin, null, 4));
