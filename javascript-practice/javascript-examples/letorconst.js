e7bb193148808be50e17985ce97e0b1d9b171361
umairx97 <umair.bajwa@hotmail.com>
2019-05-04 20:01:50 +0500

    Added let or const

---
+++ javascript-practice/javascript-examples/letorconst.js
@@ -0,0 +1,87 @@
+// Try one part at a time
+
+// const myname = 'Umair'; // Declare and Initialize variable
+// console.log(myname)     // result: Umair
+
+/************************************************** */
+// const myname        // Declare variable
+// myname = 'Umair';   // Initialize variable
+// console.log(myname) // RESULT: Missing initializer in const declaration
+
+/************************************************** */
+// myname = 'Umair';   // Initialize variable
+// console.log(myname) // RESULT: Missing initializer in const declaration
+// const myname        // Declare variable
+
+/************************************************** */
+// myname = 'Umair';    // Initialize variable
+// const myname         // Declare variable
+// console.log(myname)  // RESULT: Missing initializer in const declaration
+
+
+
+// ************************************************** 
+// Try any part one at a time
+
+// var x = 10;
+// console.log(x)       // Result: 10
+// {
+//     let x = 2;
+//     console.log(x)   // Result: 2
+// }
+// console.log(x)       // Result: 10
+
+
+/************************************************** */
+
+// let myname = "Umair"
+
+// console.log(myname)  //Result: Umair
+
+// function getName() {
+//     let myname = "Ahmed"
+//     console.log(myname) //Result: Ahmed
+// }
+
+// getName()
+
+// console.log(myname)  //Result: Umair
+
+
+
+/************************************************** */
+
+
+
+
+// let myname          // Declare variable
+// myname = 'Umair';   // Initialize variable
+// console.log(myname) // result: Umair
+
+
+
+/************************************************** */
+
+// let myname          // Declare variable
+// console.log(myname) // result: Undefined
+// myname = 'Umair';   // Initialize variable
+
+
+
+/************************************************** */
+
+
+// myname = 'Umair';   // Initialize variable
+// console.log(myname) // result: ERROR: myname is not defined
+// let myname          // Declare variable
+
+
+
+/************************************************** */
+
+// myname = 'Umair';   // Initialize variable
+// let myname          // Declare variable
+// console.log(myname)// result: ERROR: myname is not defined
+
+
+
